5428d71bf0a9d3ba7f111936cdd6374fe1aafbae
omarnaboush122 <omarnaboush89@gmail.com>
2023-03-23 15:00:59 +0200

    add custom styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      "dark-cyan": "#0E8784",
+      "dark-grey-blue": "#333D4B",
+      "pale-orange": "#FDD6BA",
+      "light-cream": "#FEFCF7",
+      "grey": "#83888F"
+    },
+    fontFamily: {
+      barlow: ['Barlow', 'sans-serif'],
+      Fraunces: ['Fraunces', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
