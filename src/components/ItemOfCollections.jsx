fc9c8d1c0d493f5c49f65af86a91b66eed35e9a1
omarnaboush122 <omarnaboush89@gmail.com>
2023-03-24 17:24:40 +0200

    style collections section at tablet screen

--- src/components/ItemOfCollections.jsx
+++ src/components/ItemOfCollections.jsx
@@ -1,11 +1,13 @@
 const Item = ({ title, img, desc }) => {
   return (
-    <article className="flex flex-col justify-center items-center">
-      <img src={img} alt={title} className="w-[200px]" />
-      <h3 className="text-dark-grey-blue font-Fraunces text-2xl font-black mt-6 mb-4">
+    <article className="flex flex-col justify-center items-center md:flex-row md:items-start md:justify-between md:gap-9">
+      <img src={img} alt={title} className="w-[250px]" />
+      <div>
+      <h3 className="text-dark-grey-blue font-Fraunces text-2xl font-black mt-6 mb-4 md:mb-6">
         {title}
       </h3>
       <p className="text-dark-grey-blue">{desc}</p>
+      </div>
     </article>
   );
 };
--- src/components/OurCollection.jsx
+++ src/components/OurCollection.jsx
@@ -1,6 +1,7 @@
 import Gran from "../assets/home/desktop/image-gran-espresso.png";
 import Danche from "../assets/home/desktop/image-danche.png";
 import Piccollo from "../assets/home/desktop/image-piccollo.png";
+import Planalto from "../assets/home/desktop/image-planalto.png";
 import ItemOfCollections from "./ItemOfCollections";
 
 const OurCollection = () => {
@@ -13,27 +14,35 @@ const OurCollection = () => {
     },
     {
       id: 2,
+      title: "Planalto",
+      desc: "Brazilian dark roast with rich and velvety body, and hints of fruits and nuts",
+      img: Planalto,
+    },
+    {
+      id: 3,
       title: "Piccollo",
       desc: "Mild and smooth blend featuring notes of toasted almond and dried cherry",
       img: Piccollo,
     },
     {
-      id: 3,
+      id: 4,
       title: "Danche",
       desc: "Ethiopian hand-harvested blend densely packed with vibrant fruit notes",
       img: Danche,
     },
   ];
 
-  const collectionsElements = collections.map((item) => <ItemOfCollections key={item.id} {...item} />);
+  const collectionsElements = collections.map((item) => (
+    <ItemOfCollections key={item.id} {...item} />
+  ));
   return (
-    <section className="my-[120px]">
-      <div className="collection w-full flex justify-center items-center px-11">
-        <h1 className="text-4xl font-Fraunces font-black text-grey">
+    <section className="relative my-[120px]">
+      <div className="collection w-full flex justify-center items-center px-11 md:absolute md:-top-16">
+        <h1 className="text-4xl font-Fraunces font-black text-grey opacity-20 md:text-8xl">
           our collection
         </h1>
       </div>
-      <div className="grid grid-cols-1 gap-12 max-w-[300px] mx-auto text-center mt-10">
+      <div className="grid grid-cols-1 gap-12 relative max-w-[300px] mx-auto text-center mt-10 md:max-w-[600px] md:text-left">
         {collectionsElements}
       </div>
     </section>
