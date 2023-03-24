72ec61964a3f7ced873c92aa84e319f2a909dab9
omarnaboush122 <omarnaboush89@gmail.com>
2023-03-24 14:35:34 +0200

    add first item to collection section

--- src/components/OurCollection.jsx
+++ src/components/OurCollection.jsx
@@ -1,11 +1,25 @@
-
+import Gran from "../assets/home/desktop/image-gran-espresso.png";
 
 const OurCollection = () => {
   return (
-    <section>
-      
+    <section className="my-[120px]">
+      <div className="collection w-full flex justify-center items-center px-11">
+        <h1 className="text-4xl font-Fraunces font-black text-grey">
+          our collection
+        </h1>
+      </div>
+      <div className="grid grid-cols-1 max-w-[300px] mx-auto text-center mt-10">
+        <article className="flex flex-col justify-center items-center">
+          <img src={Gran} alt="gran-espresso" className="w-[200px]" />
+          <h3 className="text-dark-grey-blue font-Fraunces text-2xl font-black mt-6 mb-4">Gran Espresso</h3>
+          <p className="text-dark-grey-blue">
+            Light and flavorful blend with cocoa and black pepper for an intense
+            experience
+          </p>
+        </article>
+      </div>
     </section>
   );
-}
+};
 
 export default OurCollection;
