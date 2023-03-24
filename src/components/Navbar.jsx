9d30533812b6e5d1aaf213ff6ce38fa037e73f66
omarnaboush122 <omarnaboush89@gmail.com>
2023-03-24 09:58:33 +0200

    add react router dom and nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import Hamburger from "../assets/shared/mobile/icon-hamburger.svg";
 import Logo from "../assets/shared/desktop/logo.svg";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -7,6 +8,34 @@ const Navbar = () => {
       <div className="w-full px-6 py-8 flex justify-between items-center">
         <img src={Logo} alt="logo" className="w-40" />
         <img src={Hamburger} alt="hamburger" className="cursor-pointer" />
+      </div>
+      <div className="mt-10">
+        <ul className="flex flex-col justify-center items-center gap-8 py-10">
+          <li>
+            <Link
+              to="/"
+              className="text-dark-grey-blue font-Fraunces text-2xl font-black capitalize"
+            >
+              home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              className="text-dark-grey-blue font-Fraunces text-2xl font-black capitalize"
+            >
+              about us
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/plan"
+              className="text-dark-grey-blue font-Fraunces text-2xl font-black capitalize"
+            >
+              create your plan
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
