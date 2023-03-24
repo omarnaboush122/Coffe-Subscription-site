d920a5eb89a504933539b8841905e7fc7c67d5ba
omarnaboush122 <omarnaboush89@gmail.com>
2023-03-24 11:29:31 +0200

    add header to mobile screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header>
-      <div>
-        
+    <header className="header w-[327px] h-[500px] mx-auto my-3 rounded-md">
+      <div className="flex flex-col items-center justify-center text-center w-full h-full text-light-cream px-6">
+        <h1 className="text-[40px] leading-10 font-Fraunces font-black">
+          Great coffee made simple.
+        </h1>
+        <p className="text-[15px] leading-6 mt-6 mb-9 opacity-80">
+          Start your mornings with the world’s best coffees. Try our expertly
+          curated artisan coffees from our best roasters delivered directly to
+          your door, at your schedule.
+        </p>
+        <Link to="/plan">
+          <button className="bg-dark-cyan py-4 px-8 text-center rounded-md text-lg font-Fraunces font-black hover:bg-pale-orange transition-colors duration-300">
+            Create your plan
+          </button>
+        </Link>
       </div>
     </header>
   );
-}
+};
 
 export default Header;
