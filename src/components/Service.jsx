6fb4d61a593e60e80a62e19d81ed265cdfd244d6
omarnaboush122 <omarnaboush89@gmail.com>
2023-03-25 10:49:08 +0200

    style services at desktop screen

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,6 +1,6 @@
 const Service = ({ img, title, desc }) => {
   return (
-    <article className="bg-dark-cyan max-w-[300px] mx-auto h-96 px-6 flex flex-col justify-center items-center text-center gap-14 rounded-lg sm:flex-row sm:text-left sm:h-48 sm:max-w-[600px] sm:px-16">
+    <article className="bg-dark-cyan max-w-[400px] mx-auto h-96 px-6 flex flex-col justify-center items-center text-center gap-14 rounded-lg md:flex-row md:text-left md:h-48 md:max-w-[600px] md:px-16 lg:flex-col lg:text-center lg:px-12 lg:h-[380px]">
       <img src={img} alt={title} className="md:w-14" />
       <div>
         <h3 className="text-2xl text-light-cream font-Fraunces font-black mb-6">
