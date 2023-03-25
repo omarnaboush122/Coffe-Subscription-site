afb7df62840889dc139e5070c9b07c0cc837263d
omarnaboush122 <omarnaboush89@gmail.com>
2023-03-25 17:26:50 +0200

    style footer at tablet and desktop screens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,12 @@ import Logo from "../assets/shared/desktop/footer-logo.png";
 
 const Footer = () => {
   return (
-    <footer className="bg-black mt-52 mb-20 mx-6">
-      <div className="flex flex-col justify-center items-center text-center gap-12 py-14">
+    <footer className="bg-black mt-52 mb-20 mx-6 md:mx-10 lg:mx-20 lg:mb-32">
+      <div className="flex flex-col justify-center items-center text-center py-14 lg:flex-row lg:justify-between lg:py-10 lg:px-20">
         <div>
-          <img src={Logo} alt="logo" className="w-52" />
+          <img src={Logo} alt="logo" className="w-60" />
         </div>
-        <ul className="flex flex-col gap-6">
+        <ul className="flex flex-col gap-6 my-12 md:flex-row md:mb-16 lg:my-0">
           <li>
             <Link
               to="/"
@@ -35,9 +35,9 @@ const Footer = () => {
           </li>
         </ul>
         <div className="flex items-center gap-6 text-light-cream text-2xl">
-          <i class="fa-brands fa-facebook cursor-pointer hover:text-dark-cyan transition-colors duration-300"></i>
-          <i class="fa-brands fa-twitter cursor-pointer hover:text-dark-cyan transition-colors duration-300"></i>
-          <i class="fa-brands fa-instagram cursor-pointer hover:text-dark-cyan transition-colors duration-300"></i>
+          <i class="fa-brands fa-facebook cursor-pointer hover:text-pale-orange transition-colors duration-300"></i>
+          <i class="fa-brands fa-twitter cursor-pointer hover:text-pale-orange transition-colors duration-300"></i>
+          <i class="fa-brands fa-instagram cursor-pointer hover:text-pale-orange transition-colors duration-300"></i>
         </div>
       </div>
     </footer>
