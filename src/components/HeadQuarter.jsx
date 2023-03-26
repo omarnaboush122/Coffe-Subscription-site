91f78f9c68cd0727a21094b51337553416b36995
omarnaboush122 <omarnaboush89@gmail.com>
2023-03-26 11:35:32 +0300

    add all items of headquarters section

---
+++ src/components/HeadQuarter.jsx
@@ -0,0 +1,18 @@
+const HeadQuarter = ({ img, title, address, city, code, phoneNumber }) => {
+  return (
+    <article className="flex flex-col justify-center items-center">
+      <img src={img} alt="united-kingdom" />
+      <h3 className="text-dark-grey-blue text-2xl font-Fraunces font-black mt-11 mb-6">
+        {title}
+      </h3>
+      <ul className="flex flex-col gap-2">
+        <li className="text-dark-grey-blue">{address}</li>
+        <li className="text-dark-grey-blue">{city}</li>
+        <li className="text-dark-grey-blue">{code}</li>
+        <li className="text-dark-grey-blue">{phoneNumber}</li>
+      </ul>
+    </article>
+  );
+};
+
+export default HeadQuarter;
--- src/components/HeadQuarters.jsx
+++ src/components/HeadQuarters.jsx
@@ -1,25 +1,20 @@
 import UK from "../assets/about/desktop/illustration-uk.svg";
+import Canada from "../assets/about/desktop/illustration-canada.svg";
+import Australia from "../assets/about/desktop/illustration-australia.svg";
+import HeadQuarter from "./HeadQuarter";
 
 const HeadQuarters = () => {
+
   return (
     <section className="my-32 mx-6 md:mx-10 lg:mx-20">
       <div className="container mx-auto">
         <h2 className="text-grey text-2xl font-Fraunces font-black text-center mb-16">
           Our headquarters
         </h2>
-        <div className="grid grid-cols-1 text-center">
-          <article className="flex flex-col justify-center items-center">
-            <img src={UK} alt="united-kingdom" />
-            <h3 className="text-dark-grey-blue text-2xl font-Fraunces font-black mt-11 mb-6">
-              United Kingdom
-            </h3>
-            <ul className="flex flex-col gap-2">
-              <li className="text-dark-grey-blue">68 Asfordby Rd</li>
-              <li className="text-dark-grey-blue">Alcaston</li>
-              <li className="text-dark-grey-blue">SY6 1YA</li>
-              <li className="text-dark-grey-blue">+44 1241 918425</li>
-            </ul>
-          </article>
+        <div className="grid grid-cols-1 text-center gap-20">
+          <HeadQuarter img={UK} title="United Kingdom" address="68 Asfordby Rd" city="Alcaston" code="SY6 1YA" phoneNumber="+44 1241 918425"/>
+          <HeadQuarter img={Canada} title="Canada" address="1528 Eglinton Avenue" city="Toronto" code="Ontario M4P 1A6" phoneNumber="+1 416 485 2997"/>
+          <HeadQuarter img={Australia} title="Australia" address="36 Swanston Street" city="Kewell" code="Victoria" phoneNumber="+61 4 9928 3629"/>
         </div>
       </div>
     </section>
