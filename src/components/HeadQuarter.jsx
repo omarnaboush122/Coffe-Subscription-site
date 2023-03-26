1f0070a3fa632e0a8ac4a7c7058d0a9375002194
omarnaboush122 <omarnaboush89@gmail.com>
2023-03-26 11:41:21 +0300

    style headquarter section at tablet and desktop screen

--- src/components/HeadQuarter.jsx
+++ src/components/HeadQuarter.jsx
@@ -1,8 +1,8 @@
 const HeadQuarter = ({ img, title, address, city, code, phoneNumber }) => {
   return (
-    <article className="flex flex-col justify-center items-center">
+    <article className="flex flex-col justify-center items-center md:items-start">
       <img src={img} alt="united-kingdom" />
-      <h3 className="text-dark-grey-blue text-2xl font-Fraunces font-black mt-11 mb-6">
+      <h3 className="text-dark-grey-blue text-2xl font-Fraunces font-black mt-11 mb-6 md:text-3xl">
         {title}
       </h3>
       <ul className="flex flex-col gap-2">
--- src/components/HeadQuarters.jsx
+++ src/components/HeadQuarters.jsx
@@ -8,10 +8,10 @@ const HeadQuarters = () => {
   return (
     <section className="my-32 mx-6 md:mx-10 lg:mx-20">
       <div className="container mx-auto">
-        <h2 className="text-grey text-2xl font-Fraunces font-black text-center mb-16">
+        <h2 className="text-grey text-2xl font-Fraunces font-black text-center mb-16 md:text-left">
           Our headquarters
         </h2>
-        <div className="grid grid-cols-1 text-center gap-20">
+        <div className="grid grid-cols-1 text-center gap-20 md:grid-cols-3 md:gap-5 md:text-left lg:gap-10">
           <HeadQuarter img={UK} title="United Kingdom" address="68 Asfordby Rd" city="Alcaston" code="SY6 1YA" phoneNumber="+44 1241 918425"/>
           <HeadQuarter img={Canada} title="Canada" address="1528 Eglinton Avenue" city="Toronto" code="Ontario M4P 1A6" phoneNumber="+1 416 485 2997"/>
           <HeadQuarter img={Australia} title="Australia" address="36 Swanston Street" city="Kewell" code="Victoria" phoneNumber="+61 4 9928 3629"/>
