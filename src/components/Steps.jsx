a2ec3bdf3d7ab2493d311d52f0763432136be6eb
omarnaboush122 <omarnaboush89@gmail.com>
2023-04-01 16:12:29 +0300

    change steps background color at plan page

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -4,9 +4,9 @@ const Steps = () => {
   return (
     <section className="bg-black mt-14 rounded-md md:my-36 lg:mx-20">
       <div className="hidden md:block relative top-20 left-14 w-[70%] h-[2px] bg-pale-orange my-14">
-        <span className="absolute -top-[15px] left-0 w-8 h-8 bg-light-cream rounded-full border-2 border-solid border-dark-cyan"></span>
-        <span className="absolute -top-[15px] left-[50%] w-8 h-8 bg-light-cream rounded-full border-2 border-solid border-dark-cyan"></span>
-        <span className="absolute -top-[15px] left-[100%] w-8 h-8 bg-light-cream rounded-full border-2 border-solid border-dark-cyan"></span>
+        <span className="absolute -top-[15px] left-0 w-8 h-8 bg-black rounded-full border-2 border-solid border-dark-cyan"></span>
+        <span className="absolute -top-[15px] left-[50%] w-8 h-8 bg-black rounded-full border-2 border-solid border-dark-cyan"></span>
+        <span className="absolute -top-[15px] left-[98%] w-8 h-8 bg-black rounded-full border-2 border-solid border-dark-cyan"></span>
       </div>
       <div className="grid grid-cols-1 gap-14 px-6 py-20 md:grid-cols-3 md:gap-10 md:px-10 lg:gap-24 lg:pl-16">
         <PlanStep
